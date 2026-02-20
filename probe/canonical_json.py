65c2bbc8e3ad4432de361d85d965cb6a7c267b47
proFQuansistor <profquansistor101@seznam.cz>
2026-02-20 09:11:02 +0100

    Update canonical_json.py

--- probe/canonical_json.py
+++ probe/canonical_json.py
@@ -1,1 +1,10 @@
 # canonical json module
+import json
+
+def canonical_json(data):
+    return json.dumps(
+        data,
+        sort_keys=True,
+        separators=(",", ":"),
+        ensure_ascii=False,
+    )
